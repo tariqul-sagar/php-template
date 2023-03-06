5cc1f63f4418477bc15c5d8461f51618aa0babad
tisagar11 <tariqulislamsagar.cse@gmail.com>
2023-03-06 16:10:24 +0600

    Done

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Title -->
+    <title>Template</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="images/png" href="images/logo.png" sizes="16x16">
+    <!-- Fonts Link -->
+
+    <!-- Bootstrap CSS File -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <!-- Custom CSS File -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Media Query File -->
+    <link rel="stylesheet" type="text/css" href="css/media.css">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+
+    <!-- Bootstrap JS File -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- Custom JS File -->
+    <script src="js/script.js"></script>
+  </body>
+</html>
